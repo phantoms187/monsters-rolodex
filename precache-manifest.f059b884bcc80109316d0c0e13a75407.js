41bae6f7b01cd400b8e1c640fb3385ae498b7f2e
phantoms187 <dwd2@pdx.edu>
2019-11-27 15:44:54 -0800

    Updates

---
+++ precache-manifest.f059b884bcc80109316d0c0e13a75407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a139a07417d27563d106c02241b2e9ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4fe54040cfa0464dbd6a",
+    "url": "/monsters-rolodex/static/css/main.9e070c51.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4fe54040cfa0464dbd6a",
+    "url": "/monsters-rolodex/static/js/main.a109690b.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
